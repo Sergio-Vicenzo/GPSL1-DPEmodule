3df994171e6c822bc501018597cf008c37e561d2
Sergio Vicenzo <seergio.vicenzo@connect.polyu.hk>
2024-11-03 20:23:03 +0800

    Update DPE_module.m

--- Common/DPE_module.m
+++ Common/DPE_module.m
@@ -66,7 +66,7 @@ function [navSolutions]=DPE_module...
 %   with this program; if not, write to the Free Software Foundation, Inc.,
 %   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
-% Last Updated: 19 Oct 2024
+% Last Updated: 03 Nov 2024
 
 addpath include common
 
